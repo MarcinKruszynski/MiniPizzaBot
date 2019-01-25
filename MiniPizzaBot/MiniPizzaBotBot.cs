f7ddbb60ca453adfae05cd6a882938e505b381e6
MarcinKruszynski <martin.kruszynski@gmail.com>
2019-01-25 01:00:01 +0100

    first check-in

---
+++ MiniPizzaBot/MiniPizzaBotBot.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+
+namespace MiniPizzaBot
+{
+    public class MiniPizzaBotBot : IBot
+    {        
+        private readonly ILogger _logger;
+
+        private readonly UserState _userState;
+        private readonly ConversationState _conversationState;
+        private readonly BotServices _services;
+
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        /// <param name="accessors">A class containing <see cref="IStatePropertyAccessor{T}"/> used to manage state.</param>
+        /// <param name="loggerFactory">A <see cref="ILoggerFactory"/> that is hooked to the Azure App Service provider.</param>
+        /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-2.1#windows-eventlog-provider"/>
+        public MiniPizzaBotBot(BotServices services, UserState userState, ConversationState conversationState, ILoggerFactory loggerFactory)
+        {
+            if (loggerFactory == null)
+            {
+                throw new System.ArgumentNullException(nameof(loggerFactory));
+            }
+
+            _logger = loggerFactory.CreateLogger<MiniPizzaBotBot>();
+            _logger.LogTrace("Turn start.");
+
+            _services = services ?? throw new ArgumentNullException(nameof(services));
+            _userState = userState ?? throw new ArgumentNullException(nameof(userState));
+            _conversationState = conversationState ?? throw new ArgumentNullException(nameof(conversationState));
+        }
+
+        /// <summary>
+        /// Every conversation turn for our Echo Bot will call this method.
+        /// There are no dialogs used, since it's "single turn" processing, meaning a single
+        /// request and response.
+        /// </summary>
+        /// <param name="turnContext">A <see cref="ITurnContext"/> containing all the data needed
+        /// for processing this conversation turn. </param>
+        /// <param name="cancellationToken">(Optional) A <see cref="CancellationToken"/> that can be used by other objects
+        /// or threads to receive notice of cancellation.</param>
+        /// <returns>A <see cref="Task"/> that represents the work queued to execute.</returns>
+        /// <seealso cref="BotStateSet"/>
+        /// <seealso cref="ConversationState"/>
+        /// <seealso cref="IMiddleware"/>
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var activity = turnContext.Activity;
+
+            if (activity.Type == ActivityTypes.Message)
+            {
+                // Echo back to the user whatever they typed.
+                var responseMessage = $"You sent '{activity.Text}'\n";
+                await turnContext.SendActivityAsync(responseMessage);
+            }
+            else if (activity.Type == ActivityTypes.ConversationUpdate)
+            {
+                if (activity.MembersAdded != null)
+                {                    
+                    foreach (var member in activity.MembersAdded)
+                    {
+                        
+                        if (member.Id != activity.Recipient.Id)
+                        {
+                            //var welcomeCard = CreateAdaptiveCardAttachment();
+                            //var response = CreateResponse(activity, welcomeCard);
+                            //await dc.Context.SendActivityAsync(response);
+
+                            var responseMessage = $"Hi\n";
+                            await turnContext.SendActivityAsync(responseMessage);
+                        }
+                    }
+                }
+            }
+
+            await _conversationState.SaveChangesAsync(turnContext);
+            await _userState.SaveChangesAsync(turnContext);
+        }
+    }
+}
